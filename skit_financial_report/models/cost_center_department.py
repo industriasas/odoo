201e34cbc2baa62be27369de519d9e45d0a80a94
Carlos Paz <phoenixsampras@gmail.com>
2020-03-12 17:17:13 -0400

    Pivote 1.4

--- skit_financial_report/models/cost_center_department.py
+++ skit_financial_report/models/cost_center_department.py
@@ -5,11 +5,11 @@ from odoo import models, fields
 class CostCenter(models.Model):
     _name = "tf.cost.center"
 
-    name = fields.Char('Cost Center')
+    name = fields.Char('Centro de Costo')
     # department_ids = fields.One2many('tf.department', 'cost_center_id', string='Department')
 
 class Department(models.Model):
     _name = "tf.department"
 
-    name = fields.Char('Department')
+    name = fields.Char('Departmento')
     # cost_center_id = fields.Many2one('tf.cost.center')
