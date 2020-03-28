bd9b970cf9c17fde5940d2466fc68d678c58b62e
phoenixsampras <phoenixsampras@gmail.com>
2020-03-27 23:24:34 -0400

    filtros para el balance

---
+++ as_balance_sheet_accounting/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Ahorasoft Quick Accounting Informations',
+    'version': '1.0',
+    'summary': 'View and create reports',
+    'category': 'Accounting/Accounting',
+    'description': """  """,
+    'website': 'http://www.ahorasoft.com/',
+    'license': 'Other proprietary',
+    'depends': ['account_accountant', 'account_reports','skit_financial_report'],
+    'data': [
+        'data/data.xml',
+        'views/assets.xml',
+        'views/account_report_view.xml',
+        'views/search_template_view.xml',
+
+    ],
+    'qweb': [
+    ],
+    'demo': [
+    ],
+    'css': [
+    ],
+}
